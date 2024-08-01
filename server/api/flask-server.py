76e0d18f410a2af6ba5ad7ccf5b6ed1effa2427a
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2024-08-01 03:25:03 -0400

    seperated helper functions

--- server/api/flask-server.py
+++ server/api/flask-server.py
@@ -1,30 +1,8 @@
 from flask import Flask, request, jsonify
-# from ultralytics import YOLO
-from PIL import Image
-import io
-import numpy as np
-# import cv2
+
+from utils import runYOLO
 
 app = Flask(__name__)
-
-# Load the model
-"""
-model = YOLO('models/bestonnx.pt')
-    
-def runYOLO(img):
-    results = model(img, conf=0.4, iou=0.4)
-    bounding_boxes = []
-
-    # loop through results
-    for result in results:
-        boxes = result.boxes.cpu().numpy()
-    
-        # convert into cv2 rectangle
-        for xyxy in boxes.xyxy:
-            bounding_boxes.append(xyxy)
-        
-    return bounding_boxes
-"""
 
 @app.route('/response', methods=['POST'])
 def response():
@@ -35,24 +13,21 @@ def response():
     return jsonify({'response': 'unknown message'})
 
 
-    
-
 @app.route('/predict', methods=['POST'])
 def predict():
     # lets get this workign in a local server first before doing kubernetes
     # have our react native app make a post request to this to run the model
     # figure out how to send get photos via post request 
     print(request)
-    if 'file' not in request.files:
+    print('help')
+    if 'image' not in request.files:
         return jsonify({"error": "No file part"})
-    file = request.files['file']
-    if file.filename == '':
+    image = request.files['image']
+    if image.filename == '':
         return jsonify({"error": "No selected file"})
-    if file:
-        image = Image.open(io.BytesIO(file.read()))
-        # bouinding_boxes = runYOLO(image)
-        result = {"data": [{"hello": "object", "confidence": 0.9}]}
-        return jsonify(result)
+    print(image)
+    result = runYOLO(image)
+    return jsonify(result)
     # after running img in run yolo send processed bounding boxes back
    
 
---
+++ server/api/utils.py
@@ -0,0 +1,46 @@
+from ultralytics import YOLO
+from PIL import Image
+import io
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+model = YOLO('models/bestonnx.pt')
+
+def runYOLO(image):
+    # 1. Dockerify
+    # 2. Set up kubernetes config
+    # 3. upload to cloud platform
+    
+    img_byte_arr = io.BytesIO()
+    Image.open(image).save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    np_img = np.frombuffer(img_byte_arr, np.uint8)
+    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    results = model(img, conf=0.4, iou=0.4)
+
+    annotated_img = results[0].plot()
+    # Save the annotated image
+    if os.path.exists('annotated_image.jpg'):
+            os.remove('annotated_image.jpg')
+    cv2.imwrite('annotated_image.jpg', results[0].plot())
+
+    # Load and display the saved image using matplotlib
+    plt.figure()
+    annotated_img = cv2.imread('annotated_image.jpg')
+    plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB))
+    plt.title("Annotated Image with Detections")
+    plt.show()
+
+    bounding_boxes = []
+    # loop through results
+    for result in results:
+        boxes = result.boxes.cpu().numpy()
+    
+        # convert into cv2 rectangle
+        for xyxy in boxes.xyxy:
+            bounding_boxes.append(xyxy)
+        
+    return bounding_boxes
