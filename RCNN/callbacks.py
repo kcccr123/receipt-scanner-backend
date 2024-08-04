4b5ce0bdfa87590bf66626ebcd266c3ca7dd8c72
Gary Guo <garyguo2004@gmail.com>
2024-08-04 01:25:53 -0400

    edits to some files

--- RCNN/callbacks.py
+++ RCNN/callbacks.py
@@ -281,48 +281,35 @@ class Model2onnx(Callback):
         
         # convert the model to ONNX format
         dummy_input = torch.randn((1,) + self.input_shape[1:])
-
-        # handle initial states for LSTM
-        lstm_hidden_size = self.model.model.lstm0.hidden_size
-        print(lstm_hidden_size)
-        h0 = torch.randn(2, 1, lstm_hidden_size)  # 2 for bidirectional
-        c0 = torch.randn(2, 1, lstm_hidden_size)  # 2 for bidirectional
         
-        dynamic_axes = {
-            "input": {0: "batch_size", 1: "sequence_length"},
-            "output": {0: "batch_size"},
-            "h0": {1: "batch_size"},
-            "c0": {1: "batch_size"}
-        }
-
         print(dummy_input.size())
         # Export the model
         torch.onnx.export(
             self.model.model,               
-            (dummy_input, h0, c0),                         
+            dummy_input,                         
             self.onnx_model_path,   
             export_params=self.export_params,        
             opset_version=self.opset_version,          
             do_constant_folding=self.do_constant_folding,  
-            input_names = self.input_names + ["h0", "c0"],   
+            input_names = self.input_names,   
             output_names = self.output_names, 
-            dynamic_axes = dynamic_axes,
+            dynamic_axes = self.dynamic_axes,
             )
 
         if self.verbose:
             self.logger.info(f"Model saved to {self.onnx_model_path}")
 
-        # if self.metadata and isinstance(self.metadata, dict):
+        if self.metadata and isinstance(self.metadata, dict):
 
-        #     onnx_model = onnx.load(self.onnx_model_path)
-        #     # Add the metadata dictionary to the model's metadata_props attribute
-        #     for key, value in self.metadata.items():
-        #         meta = onnx_model.metadata_props.add()
-        #         meta.key = key
-        #         meta.value = str(value)
+            onnx_model = onnx.load(self.onnx_model_path)
+            # Add the metadata dictionary to the model's metadata_props attribute
+            for key, value in self.metadata.items():
+                meta = onnx_model.metadata_props.add()
+                meta.key = key
+                meta.value = str(value)
 
-        #     # Save the modified ONNX model
-        #     onnx.save(onnx_model, self.onnx_model_path)
+            # Save the modified ONNX model
+            onnx.save(onnx_model, self.onnx_model_path)
 
         # place model back to original device
         self.model.model.to(self.model.device)
--- RCNN/modelArc.py
+++ RCNN/modelArc.py
@@ -53,7 +53,7 @@ class CRNN(nn.Module):
         self.lstm0 = nn.LSTM(64, 128, bidirectional = True, num_layers = 1, batch_first = True)
         self.fc0 = nn.Linear(128*2, max_chars + 1)
     
-    def forward(self, input: torch.Tensor, h0=None, c0=None) -> torch.Tensor:
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         #input_norm = input/255.0
         input_norm = input.permute(0, 3, 1, 2)
 
@@ -70,10 +70,55 @@ class CRNN(nn.Module):
         x = self.r9(x)
         x = x.reshape(x.size(0), -1, x.size(1))
 
-        if h0 is None or c0 is None:
-            x, _ = self.lstm0(x)
-        else:
-            x, _ = self.lstm0(x, (h0, c0))
+        x, _ = self.lstm0(x)
+
+        x = self.dropout(x)
+        x = self.fc0(x)
+        x = F.log_softmax(x, 2)
+
+        return x
+
+
+class CRNN2(nn.Module):
+    def __init__(self, max_chars: int):
+        super(CRNN2, self).__init__()
+        self.name = CRNN2
+        
+        self.dropout = nn.Dropout(0.3)
+        self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 1)
+        
+        self.c0 = nn.Conv2d(3, 16, 3)
+        self.r1 = resblk(16, 16)
+        self.r2 = resblk(16, 16, skip = False)
+        self.r3 = resblk(16, 32, stride = 2)
+        self.r4 = resblk(32, 32, skip = False)
+        self.r5 = resblk(32, 64, stride = 2)
+        self.r6 = resblk(64, 64)
+        self.r7 = resblk(64, 64, stride = 2)
+        self.r8 = resblk(64, 64, skip = False)
+        self.r9 = resblk(64, 64, skip = False)
+
+        self.lstm0 = nn.LSTM(64, 128, bidirectional = True, num_layers = 1, batch_first = True)
+        self.fc0 = nn.Linear(128*2, max_chars + 1)
+    
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        #input_norm = input/255.0
+        input_norm = input.permute(0, 3, 1, 2)
+
+        x = F.relu(self.c0(input_norm))
+        x = self.pool(x)
+        x = self.r1(x)
+        x = self.r2(x)
+        x = self.r3(x)
+        x = self.r4(x)
+        x = self.r5(x)
+        x = self.r6(x)
+        x = self.r7(x)
+        x = self.r8(x)
+        x = self.r9(x)
+        x = x.reshape(x.size(0), -1, x.size(1))
+
+        x, _ = self.lstm0(x)
 
         x = self.dropout(x)
         x = self.fc0(x)
