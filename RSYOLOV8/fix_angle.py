c5f67c4ca781cb03cfdd1377303654ada7121afb
Gary Guo <garyguo2004@gmail.com>
2024-08-31 00:42:25 -0400

    function now works for part receipt in images

--- RSYOLOV8/fix_angle.py
+++ RSYOLOV8/fix_angle.py
@@ -25,33 +25,44 @@ def approximate_contour(contour):
 def get_receipt_contour(contours):    
     for c in contours:
         approx = approximate_contour(c)
+        print(len(approx))
         # four points = rectangle
         if len(approx) == 4:
+            print(f"taken:{len(approx)}")
             return approx
 
 def wrap_perspective(img, rect):
-    # top left, top right, bottom right, bottom left
+    # unpack rectangle points: top left, top right, bottom right, bottom left
     (tl, tr, br, bl) = rect
-    # width of new image
+    # compute the width of the new image
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    # height of new image
+    # compute the height of the new image
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    # take the maximum of the width and height values to reach
+    # our final dimensions
     maxWidth = max(int(widthA), int(widthB))
     maxHeight = max(int(heightA), int(heightB))
+    # destination points which will be used to map the screen to a "scanned" view
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype = "float32")
+    # calculate the perspective transform matrix
     M = cv2.getPerspectiveTransform(rect, dst)
+    # warp the perspective to grab the screen
     return cv2.warpPerspective(img, M, (maxWidth, maxHeight))
 
 def bw_scanner(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     T = threshold_local(gray, 21, offset = 5, method = "gaussian")
     return (gray > T).astype("uint8") * 255
+
+def add_border(image, border_size=2):
+    color = [0, 0, 0] 
+    return cv2.copyMakeBorder(image, border_size, border_size, 0, 0, cv2.BORDER_CONSTANT, value=color)
 
 """CALL THIS FUCTION BELOW, WHERE image = cv2.imread(path) AND WILL RETURN AN ARRAY THAT YOU CAN USE Image.fromarray TO TURN TO IMAGE AND SAVE"""
 def fix_angle(image):
@@ -70,24 +81,21 @@ def fix_angle(image):
         return rect / resize_ratio
 
     image = opencv_resize(image, resize_ratio)
-    # Convert to grayscale 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    plot_gray(gray)
-    # Get rid of noise 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # Detect white regions
-    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-    dilated = cv2.dilate(blurred, rectKernel)
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    dilated = cv2.dilate(blurred, rectKernel, iterations=4)
+    dilated = add_border(dilated)
+    edged = cv2.Canny(dilated, 75, 200, apertureSize=3)
 
-    edged = cv2.Canny(dilated, 100, 200, apertureSize=3)
-    plot_gray(edged)
-    # Detect all contours 
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Get 10 largest contours
-    largest_contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+
+    contours, hierarchy = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    filtered_contours = [c for c in contours if cv2.contourArea(c) > 1000]
+    largest_contours = sorted(filtered_contours, key = cv2.contourArea, reverse = True)[:10]
     receipt_contour = get_receipt_contour(largest_contours)
-    image_with_receipt_contour = cv2.drawContours(image.copy(), [receipt_contour], -1, (0, 255, 0), 2)
-    plot_rgb(image_with_receipt_contour)
+
     result = wrap_perspective(original.copy(), contour_to_rect(receipt_contour))
     greyscale_output = bw_scanner(result)
     return greyscale_output
